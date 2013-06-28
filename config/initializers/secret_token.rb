06d447424cae6d037578b2290e4c13ad1df263ce
Bartuz <iamnotawowplayer@gmail.com>
2013-06-28 00:09:43 -0400

    Edit gemfile and init new rail project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Try::Application.config.secret_key_base = 'e6c29d10603b6fc5413b42e4d70b3d6e77ef9f27fd7c57cb121a3568094297ef3ad83f1e11e066e10e238661c75d1a9bad88ea22b74c51898747586c413131b5'
